6be1937fe90d4c9fa0c89675fcc3b29ce4614e98
gustavodemari <guhdemari@gmail.com>
2014-08-01 04:15:41 -0300

    adjusting port

--- configs/config.js
+++ configs/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   server : {
     HOST : process.env.NODEJS_HOST || 'localhost',
-    PORT : process.env.NODEJS_PORT || 5000,
+    PORT : process.env.PORT || 5000,
     CORS_SETUP : function(req, res, next) {
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "X-Requested-With");
